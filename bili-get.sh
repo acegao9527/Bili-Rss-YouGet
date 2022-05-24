e1818aa4cc5011a5896c5ef7e4844547151a2dc8
Ace Lee <acelee9527@icloud.com>
2022-05-24 20:58:46 +0800

    update config dir

--- bili-get.sh
+++ bili-get.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 you=/var/services/homes/MyAdmin/.local/bin/you-get
-confdir=/var/services/homes/MyAdmin/workconfig/bili-conf
+confdir=/volume1/docker/bili-rss-get
 downloaddir=/volume3/迅雷/下载B站
 myrss=http://192.168.31.3:1200/bilibili/fav/1924937/1622036037
+
 cookiedir=$confdir/cookies.txt
+datedir=$confdir/date.txt
 
 content=$(wget $myrss -q -O -)
 
@@ -12,12 +14,12 @@ subpubdate=${content#*<pubDate>}
 pubdate=${subpubdate%%</pubDate>*}
 
 #如果时间戳记录文本不存在则创建
-if [ ! -f "$confdir/date.txt" ];then
-    echo 313340 > $confdir/date.txt
+if [ ! -f "$datedir" ];then
+    echo 313340 > $datedir
 fi
 
 #获得上一个视频的时间戳
-olddate=$(cat $confdir/date.txt)
+olddate=$(cat $datedir)
 
 #判断当前时间戳和上次记录是否相同，相同则代表列表未更新
 if [ "$pubdate" == "$olddate" ];then
@@ -33,7 +35,7 @@ echo "---------------------开始下载"
 echo "$you -k -l -c $cookiedir -o $downloaddir $link"
 $you -k -l -c $cookiedir -o $downloaddir $link
 if [ $? -eq 0 ];then
-    echo $pubdate > $confdir/date.txt
+    echo $pubdate > $datedir
 fi
 echo "---------------------下载成功"
 
